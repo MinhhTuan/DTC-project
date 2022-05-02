530d70db533c270a0f002a1c713572707d3360a0
MinhhTuan <nguyenducminhtuan100@gmail.com>
2022-05-02 12:12:26 +0700

    fix connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var Brand = require("./../models/brand");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  Brand.getAll(function (err, rows) {
+    if (err) {
+      res.json({
+        code: -1,
+        data: err,
+        msg: ""
+      });
+    } else {
+      res.json({
+        code: 1,
+        data: rows,
+        msg: ""
+      });
+    }
+  });
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
