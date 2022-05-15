14eea7b55521e5ba26b3c14e8c988560bcd0d2fc
MinhhTuan <nguyenducminhtuan100@gmail.com>
2022-05-15 11:27:20 +0700

    change code structure

--- models/solution.js
+++ models/solution.js
@@ -1,9 +1,19 @@
-let db = require('../data/database')
+const {Sequelize} = require('sequelize')
+const db = require('../util/database')
 
-const SolutionModel = {
-  getAll:function(callback){
-		return db.query("Select * FROM solution", callback);
+const Solution = db.define('solution', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true
 	},
-}
+	error_code: {
+		type: Sequelize.STRING
+	},
+	solution: {
+		type: Sequelize.STRING
+	},
+})
 
-module.exports = SolutionModel
+module.exports = Solution
