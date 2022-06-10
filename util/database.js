35c2d92440a88dcc2fb29f3e162949f60a3e7cde
MinhhTuan <nguyenducminhtuan100@gmail.com>
2022-06-10 21:54:25 +0700

    rebase branch main

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require('sequelize')
-const dbConfig = require('../config/db.config')
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    dialect: dbConfig.dialect,
-    host: dbConfig.HOST
-})
-
-module.exports = sequelize
