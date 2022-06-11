2e6533ff48d5d5218c854cd43d1197a679f254df
MinhhTuan <nguyenducminhtuan100@gmail.com>
2022-06-11 18:05:59 +0700

    change port

--- util/database.js
+++ util/database.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize')
 const dbConfig = require('../config/db.config')
 const sequelize = new Sequelize('dtc', 'root', '', {
     dialect: dbConfig.dialect,
-    host: dbConfig.HOST,
-    port: dbConfig.port
+    host: '0.tcp.ap.ngrok.io',
+    port: 10568
 })
 
 module.exports = sequelize
