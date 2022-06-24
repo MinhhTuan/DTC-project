6ab032365c3c3bc804e8e0525162418da0296d9c
Nguyen Duc Thinh <thinhnd194@gmail.com>
2022-06-25 01:14:51 +0700

    add code

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -1,35 +1,61 @@
-const { redirect } = require('express/lib/response')
-const Brand = require('../models/brand')
-const Model = require('../models/model')
-const Solution = require('../models/solution')
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const Brand = require('../models/brand');
+const Model = require('../models/model');
+const Solution = require('../models/solution');
 
 exports.getBrands = (req, res, next) => {
-    const years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2018', '2019', '2020', '2021']
-    Brand.findAll().then(
-        (brands) => res.render('index', {
-            'brands': brands,
-            'years': years
-        })
-    ).catch(err => console.log(err))
-}
+	const years = [
+		'2010',
+		'2011',
+		'2012',
+		'2013',
+		'2014',
+		'2015',
+		'2016',
+		'2017',
+		'2018',
+		'2018',
+		'2019',
+		'2020',
+		'2021',
+	];
+	Brand.findAll()
+		.then((brands) =>
+			res.render('index', {
+				brands: brands,
+				years: years,
+			})
+		)
+		.catch((err) => console.log(err));
+};
 
 exports.getModels = (req, res, next) => {
-    Model.findAll({
-        where: {
-            brandId: req.params.brandId
-        }
-    }).then(
-        (models) => res.json(models)
-    ).catch(err => console.log(err))
-}
+	Model.findAll({
+		where: {
+			brandId: req.params.brandId,
+		},
+	})
+		.then((models) => res.json(models))
+		.catch((err) => console.log(err));
+};
+
+exports.getErrorCodes = (req, res, next) => {
+	Solution.findAll({
+		where: {
+			modelId: req.params.modelId,
+		},
+	})
+		.then((error_codes) => res.json(error_codes))
+		.catch((err) => console.log(err));
+};
 
 exports.getResult = (req, res, next) => {
-    Solution.findOne({
-        where: {
-            error_code: req.query.error_code,
-            modelId: req.query.model_id
-        }
-    }).then(
-        (solution) => res.json(solution)
-    ).catch(err => console.log(err))
-}
+	Solution.findOne({
+		where: {
+			id: req.query.error_code
+		},
+	})
+		.then((solution) => res.json(solution))
+		.catch((err) => console.log(err));
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const indexController = require('../controllers/index_controller')
 
 router.get('/', indexController.getBrands)
 router.get('/models/:brandId', indexController.getModels)
+router.get('/errors/:modelId', indexController.getErrorCodes)
 router.get('/result', indexController.getResult)
 
 module.exports = router
